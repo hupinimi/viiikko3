606e82c4c794178d181464e1dcbdb3c3dfe13ccd
hupinimi <77345156+hupinimi@users.noreply.github.com>
2021-03-15 21:25:48 +0200

    Add files via upload

---
+++ Bottle.java
@@ -0,0 +1,42 @@
+package luento3;
+
+public class Bottle {
+	
+    private String name = "Pepsi Max";
+    private String manufacturer = "Pepsi";
+    private double total_energy = 0.3;
+    private double size = 0.5;
+    private double price = 1.80;
+    
+   
+    
+    
+    
+    public Bottle(){
+    	
+    }
+    
+    
+    public Bottle(String name, String manufacturer, float total_energy, float size, float price){}
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public String getManufacturer() {
+    	return manufacturer;
+    }
+    
+    public double getEnergy() {
+    	return total_energy;
+    }
+    
+    public double getSize() {
+    	return size;
+    }
+    
+    public double getPrice() {
+    	return price;
+    }
+
+}
---
+++ Mainclass.java
@@ -0,0 +1,14 @@
+package luento3;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		BottleDispenser bottlebud = BottleDispenser.getBottleDispenser();
+		
+
+	}
+
+}
